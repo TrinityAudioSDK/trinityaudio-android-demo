f39d0bdc342d28271bedaffc30d0219d1cb4c27f
Shlomi Sutton <shlomisut@users.noreply.github.com>
2023-12-04 17:55:59 +0200

    Update app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,6 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation("ai.trinityaudio:trinity-player:0.0.3")
+    implementation("ai.trinityaudio:trinity-player:0.1.16")
 
 }
