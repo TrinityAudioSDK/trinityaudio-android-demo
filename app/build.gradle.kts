36f33fa78be2e0d7c4fa64659a57978de0a3f902
duyhungtnn <duyhung71089@gmail.com>
2024-08-27 16:16:17 +0700

    feat: demo new apis with auto play (#3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,18 +5,21 @@ plugins {
 
 android {
     namespace = "ai.trinityaudio.sdk.sample"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "ai.trinityaudio.sdk.sample"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-
+    buildFeatures {
+        compose = false
+        viewBinding = true
+    }
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -36,13 +39,16 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
-    implementation("ai.trinityaudio:trinity-player:1.0.17")
+    implementation("ai.trinityaudio:trinity-player:1.0.48")
+
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
 }
--- app/src/main/java/ai/trinityaudio/sdk/sample/MainActivity.kt
+++ app/src/main/java/ai/trinityaudio/sdk/sample/MainActivity.kt
@@ -1,58 +1,17 @@
 package ai.trinityaudio.sdk.sample
 
-import ai.trinityaudio.sdk.TrinityAudio
-import ai.trinityaudio.sdk.TrinityPlayerListener
-import ai.trinityaudio.sdk.TrinityPlayerView
-import ai.trinityaudio.sdk.TrinityStates
-
-import android.graphics.Point
+import android.content.pm.ApplicationInfo
 import android.os.Bundle
-import android.view.ViewGroup
-import android.widget.TextView
+import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
-import org.json.JSONObject
 
-class MainActivity : AppCompatActivity(), TrinityPlayerListener {
-    private lateinit var playerView: TrinityPlayerView
-    private lateinit var rootView: ViewGroup
-    private lateinit var eventsTextView: TextView
-    private var eventLogs = ""
-    private var trinityAudio = TrinityAudio.create(this, this)
-
-
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        playerView = findViewById(R.id.trinityPlayerView)
-        rootView = findViewById(R.id.rootView)
-        eventsTextView = findViewById(R.id.events)
-        loadTrinityPlayer()
-    }
-
-    private fun loadTrinityPlayer() {
-        val unitID = "2900004156"
-        val contentURL = "https://demo.trinityaudio.ai/general-demo/demo.html"
-        val fabViewTopLeftCoordinates = Point(50, resources.displayMetrics.heightPixels * 2 / 3)
-        trinityAudio.render(rootView, playerView, unitID, fabViewTopLeftCoordinates, contentURL)
-    }
-
-    override fun trinityOnDetectUpdateForContentHeight(
-        service: TrinityAudio,
-        height: Float,
-        state: TrinityStates,
-    ) {}
-
-    override fun trinityDidReceivePostMessage(
-        service: TrinityAudio,
-        message: Map<String, *>,
-    ) {
-        runOnUiThread {
-            try {
-                eventLogs = eventLogs.plus("\n \n").plus(JSONObject(message).toString(4))
-                eventsTextView.text = eventLogs
-            } catch (ex: Exception) {
-                ex.printStackTrace()
-            }
+        // Debug WebViews in your native Android apps using Chrome Developer Tools.
+        if (0 != applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE) {
+            WebView.setWebContentsDebuggingEnabled(true)
         }
     }
 }
