ebb3a08dd626f354feaf27c950cf36f1430dabac
duyhungtnn <duyhung71089@gmail.com>
2024-12-19 21:41:00 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
-    implementation("ai.trinityaudio:trinity-player:2.2.56")
+    implementation("ai.trinityaudio:trinity-player:2.2.57")
 
     implementation("androidx.navigation:navigation-fragment-ktx:2.8.4")
     implementation("androidx.navigation:navigation-ui-ktx:2.8.4")
